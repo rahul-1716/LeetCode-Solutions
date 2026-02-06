74cbb54c156016919f78247dddee0ab6da6f1bf8
rahul-1716 <rahulnishad6969@gmail.com>
2026-02-06 20:56:06 +0530

    3634 easy feb 6

---
+++ arrays/3634-bal-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minRemoval = function(nums, k) {
+    nums.sort((a,b) => a - b)
+
+    let left = 0;
+    let maxKeep = 1;
+
+    for(let right = 0; right < nums.length; right++){
+        while(nums[right] > nums[left] * k){
+            left++;
+        }
+        maxKeep = Math.max(maxKeep, right - left + 1)
+    }
+    return nums.length - maxKeep;
+};
+
+k = 2;
+nums = [2,1,5];
+
+console.log(minRemoval(nums, k)); // 1
