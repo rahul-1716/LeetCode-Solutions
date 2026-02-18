00eff014ed5edbd01cb5e7c2cc86fb6f4835835e
rahul-1716 <rahulnishad6969@gmail.com>
2026-02-18 22:56:56 +0530

    ALLIANCE

---
+++ arrays/693-binary-alternate.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var hasAlternatingBits = function(n) {
+    let prev = n & 1;
+    n = n >> 1;
+
+    while(n > 0){
+        let curr = n & 1;
+        if(curr === prev){
+            return false;
+        }
+        prev = curr;
+        n = n >> 1;
+    }
+    return true;
+};
