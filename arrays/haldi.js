93c8e2e9a4a7a5926b3396efd830560d0100946f
rahul-1716 <rahulnishad6969@gmail.com>
2026-03-11 11:00:00 +0530

    haldi h bro , easy

---
+++ arrays/haldi.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bitwiseComplement = function(n) {
+    if (n === 0) return 1;
+
+    let mask = 0;
+
+    while(mask < n)
+            mask = (mask << 1) | 1;
+    return mask ^ n;
+};
