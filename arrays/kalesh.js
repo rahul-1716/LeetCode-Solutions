e49f0e33e881c8f55e06b6e35cac9a7fd770dfa6
rahul-1716 <rahulnishad6969@gmail.com>
2026-03-30 22:55:02 +0530

    ooof

---
+++ arrays/kalesh.js
@@ -0,0 +1,19 @@
+var checkStrings = function(s1, s2) {
+    const even = new Array(26).fill(0);
+    const odd = new Array(26).fill(0);
+
+    for (let i = 0; i < s1.length; i++) {
+        let idx1 = s1.charCodeAt(i) - 97;
+        let idx2 = s2.charCodeAt(i) - 97;
+
+        if (i % 2 === 0) {
+            even[idx1]++;
+            even[idx2]--;
+        } else {
+            odd[idx1]++;
+            odd[idx2]--;
+        }
+    }
+
+    return even.every(v => v === 0) && odd.every(v => v === 0);
+};
