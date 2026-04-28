d77a0484b3403ab2fffcea0b1c9422607dfae5e7
rahul-1716 <rahulnishad6969@gmail.com>
2026-04-28 23:19:26 +0530

    ooooooooooooooffffffff

--- striver/playground.cpp
+++ striver/playground.cpp
@@ -1,133 +1,134 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void patternOne(int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cout<<" X ";
+void patternOne(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << " X ";
         }
-        cout<<endl;
+        cout << '\n';
     }
 }
 
-void patternTwo(int n){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j <= i; j++){
-            cout<<" X ";
+void patternTwo(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << " X ";
         }
-        cout<<endl;
+        cout << '\n';
     }
 }
 
-void patternThree(int n){
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i; j++){
-            cout<<j;
+void patternThree(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j;
         }
-        cout<<endl;
+        cout << '\n';
     }
 }
 
-void patternFour(int n){
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i; j++){
-            cout<<i;
+void patternFour(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << i;
         }
-        cout<<endl;
+        cout << '\n';
     }
 }
 
-void patternFive(int n){
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n - i + 1; j++){
-            cout<<" X ";
+void patternFive(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i + 1; j++) {
+            cout << " X ";
         }
-        cout<<endl;
+        cout << '\n';
     }
 }
 
-void patternSix(int n){
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= n - i + 1; j++){
-            cout<<j;
+void patternSix(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i + 1; j++) {
+            cout << j;
         }
-        cout<<endl;
+        cout << '\n';
     }
 }
 
-void patternSeven(int n){
-    for(int i = 0; i <n ; i++){
-        // SPACE
-        for(int j = 0; j < n - i - 1; j++){
-            cout<<" ";
+void patternSeven(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << ' ';
         }
-        //STAR
-        for(int j = 0; j < 2*i+1; j++){
-            cout<<"X";
+
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << 'X';
         }
-        //SPACE
-        for(int j = 0; j < 2 * i + 1; j++){
-            cout<<" ";
+
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << ' ';
         }
-        cout<<endl;
+        cout << '\n';
     }
 }
 
-void patternEight(int n){
-    for(int i = 0; i < n; i++){
-        //SPACE
-        for(int j = 0; j < i; j++){
-            cout<<" ";
+void patternEight(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << ' ';
         }
-        //STAR
-        for(int j = 0; j < (2 * n) - (2 * i + 1); j++){
-            cout<<"X";
+
+        for (int j = 0; j < (2 * n) - (2 * i + 1); j++) {
+            cout << 'X';
         }
-        //SPACE
-        for(int j = 0; j < i; j++){
-            cout<<"";
+
+        for (int j = 0; j < i; j++) {
+            cout << ' ';
         }
-        cout<<endl;
+        cout << '\n';
     }
 }
 
-void patternNine(int n){
+void patternNine(int n) {
     patternSeven(n);
     patternEight(n);
 }
 
-void patternTen(int n){
-    for(int i = 1; i <= 2 * n - 1; i++){
+void patternTen(int n) {
+    for (int i = 1; i <= 2 * n - 1; i++) {
         int stars = i;
-        if( i > n) stars = 2 * n - i;
-        for(int j = 1; j <= stars; j++){
-            cout<<"X";
+        if (i > n) {
+            stars = 2 * n - i;
         }
-        cout<<endl;
+
+        for (int j = 1; j <= stars; j++) {
+            cout << 'X';
+        }
+        cout << '\n';
     }
 }
+
 int main() {
+    patternOne(5);
+    cout << '\n';
+    patternTwo(5);
+    cout << '\n';
+    patternThree(5);
+    cout << '\n';
+    patternFour(5);
+    cout << '\n';
+    patternFive(5);
+    cout << '\n';
+    patternSix(5);
+    cout << '\n';
+    patternSeven(5);
+    cout << '\n';
+    patternEight(8);
+    cout << '\n';
+    patternNine(5);
+    cout << '\n';
+    patternTen(8);
+    cout << '\n';
 
-   patternOne(5);
-   cout<<endl;
-   patternTwo(5);
-   cout<<endl;
-   patternThree(5);
-   cout<<endl;
-   patternFour(5);
-   cout<<endl;
-   patternFive(5);
-   cout<<endl;
-   patternSix(5);
-   cout<<endl;
-   patternSeven(5);
-   cout<<endl;
-   patternEight(8);
-   cout<<endl;
-   patternNine(5);
-   cout<<endl;
-   patternTen(8);
-   cout<<endl;
-   return 0;
+    return 0;
 }
-
